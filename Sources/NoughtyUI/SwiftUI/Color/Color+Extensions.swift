37a76ff45c038420f012954a4d3e8f9ad2b4f170
dmitrii.sorochin <dmitrii.sorochin@mail.ebs-integrator.com>
2024-09-20 11:17:45 +0300

    added color extension from hex

--- Sources/NoughtyUI/SwiftUI/Color/Color+Extensions.swift
+++ Sources/NoughtyUI/SwiftUI/Color/Color+Extensions.swift
@@ -16,13 +16,52 @@ extension Color {
                   green: Double(green) / 255.0,
                   blue: Double(blue) / 255.0)
     }
-
-   public init(rgb: Int) {
-       self.init(
-           red: (rgb >> 16) & 0xFF,
-           green: (rgb >> 8) & 0xFF,
-           blue: rgb & 0xFF
-       )
-   }
+    
+    public init(rgb: Int) {
+        self.init(
+            red: (rgb >> 16) & 0xFF,
+            green: (rgb >> 8) & 0xFF,
+            blue: rgb & 0xFF
+        )
+    }
+    
+    public init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3:
+            (a, r, g, b) = (
+                255,
+                (int >> 8) * 17,
+                (int >> 4 & 0xF) * 17,
+                (int & 0xF) * 17
+            )
+        case 6:
+            (a, r, g, b) = (
+                255,
+                int >> 16 & 0xFF,
+                int >> 8 & 0xFF,
+                int & 0xFF
+            )
+        case 8:
+            (a, r, g, b) = (
+                int >> 24 & 0xFF,
+                int >> 16 & 0xFF,
+                int >> 8 & 0xFF,
+                int & 0xFF
+            )
+        default:
+            (a, r, g, b) = (255, 0, 0, 0)
+        }
+        
+        self.init(
+            .sRGB,
+            red: Double(r) / 255,
+            green: Double(g) / 255,
+            blue: Double(b) / 255
+        )
+    }
 }
-
