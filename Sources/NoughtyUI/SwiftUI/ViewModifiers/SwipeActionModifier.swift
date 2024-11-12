78d0f666a59e68d219f7db9504958d820c4c7976
cruasanya <98823717+cruasanya@users.noreply.github.com>
2024-11-12 15:20:13 +0200

    replace SwipeToDeleteModifier with SwipeAcrtionModifier to enable multiple multiple swipe actions

--- Sources/NoughtyUI/SwiftUI/ViewModifiers/SwipeActionModifier.swift
+++ Sources/NoughtyUI/SwiftUI/ViewModifiers/SwipeActionModifier.swift
@@ -10,69 +10,81 @@ import SwiftUI
 public enum SwipeStage: Equatable {
     case initial
     case locked
-    case commit
 }
 
-public struct DeleteViewConfig {
+public struct SwipeActionConfig {
     public let progress: CGFloat // progress to the limit offset value
     public let availableWidth: CGFloat // raw offset
     public let swipeStage: SwipeStage // stage
+    public let itemIndex: Int
+    public let actionContainerWidth: CGFloat
 }
 
-public struct SwipeToDeleteModifier<DeleteView: View>: ViewModifier {
-    public let onDelete: () -> ()
-    public let deleteView: (DeleteViewConfig) -> DeleteView
-    public let limit: CGFloat
+
+public struct SwipeActionModifier<Action: Hashable, ActionView: View>: ViewModifier {
+    public let swipeActions: [Action]
     private let actionLimit: CGFloat
-    
+    private let container: (Action, SwipeActionConfig) -> ActionView
+
+    @State private var limit: CGSize = .zero
+
     @State private var offset: CGFloat = 0
     @State private var currentTranslation: CGSize = .zero
-    
     @State private var feedback: UIImpactFeedbackGenerator?
     @State private var stage: SwipeStage = .initial
     @GestureState private var isActive = false
-    
+
     public init(
-        onDelete: @escaping () -> (),
-        deleteView: @escaping (DeleteViewConfig) -> DeleteView,
-        limit: CGFloat = 100,
-        actionLimit: CGFloat = 150
+        swipeActions: [Action],
+        actionLimit: CGFloat = 150,
+        @ViewBuilder container: @escaping (Action, SwipeActionConfig) -> ActionView
     ) {
-        self.onDelete = onDelete
-        self.deleteView = deleteView
-        self.limit = limit
-        self.actionLimit = limit + actionLimit
+        self.swipeActions = swipeActions
+        self.actionLimit = actionLimit
+        self.container = container
     }
-    
+
     private func progress(
         for offset: CGFloat,
         limit: CGFloat
     ) -> CGFloat {
-        max(min(abs(offset/limit), 1), 0.01)
+        max(min(abs(offset/limit), 1), 0)
     }
-    
+
     private var screenTranslation: CGSize {
         var size = currentTranslation
         size.width += offset
-        
         return size
     }
-    
+
     //MARK: View
     public func body(content: Content) -> some View {
         ZStack(alignment: Alignment(horizontal: .trailing, vertical: .center)) {
-            deleteView(
-                .init(
-                    progress: progress(for: screenTranslation.width, limit: limit),
-                    availableWidth: max(limit, -screenTranslation.width),
-                    swipeStage: stage
-                )
-            ).onTapGesture {
-                onDelete()
+            HStack(spacing: 0) {
+                ForEach(Array(swipeActions.enumerated()), id: \.offset) {
+                    index,
+                    swipeAction in
+                    container(
+                        swipeAction,
+                        SwipeActionConfig(
+                            progress: progress(
+                                for: screenTranslation.width,
+                                limit: limit.width
+                            ),
+                            availableWidth: max(limit.width, -screenTranslation.width),
+                            swipeStage: stage,
+                            itemIndex: index,
+                            actionContainerWidth: limit.width
+                        )
+                    )
+                }
             }
+            .observeSize($limit)
+            .frame(maxWidth: limit.width)
             content
                 .offset(x: screenTranslation.width, y: 0)
-                .gesture(
+                .layoutPriority(10)
+                .simultaneousGesture(
                     DragGesture(coordinateSpace: .local)
                         .updating($isActive) { (value, state, transaction) in
                             state = true
@@ -96,58 +108,49 @@ public struct SwipeToDeleteModifier<DeleteView: View>: ViewModifier {
 }
 
 // MARK:- Gesture management
-private extension SwipeToDeleteModifier {
+private extension SwipeActionModifier {
     func setOffset(currentTranslation: CGSize) {
         // we can't go past initial position to the right
         if currentTranslation.width + offset > 0 { return }
-        
-        // we can't go far past second limit
-        if currentTranslation.width + offset < -actionLimit - 100 { return } // minus cause we'll provide the limit as positive but moving towards negative numbers
-        
+
         // we can alter the translation to move slower/with a curve etc.
         self.currentTranslation.width = currentTranslation.width
-        
+
         // changing the stage according to current offset
         setStage(translation: currentTranslation)
     }
-    
+
     func dragEnded() {
         // resetting the active drag translation
         currentTranslation = .zero
-        
+
         switch stage {
         case .initial:
             self.offset = 0
         case .locked:
-            self.offset = -limit
-        case .commit:
-            // add animation/transition
-            self.offset = -500
-            onDelete()
+            self.offset = -limit.width
         }
     }
 }
 
 // MARK:- Stage management
-private extension SwipeToDeleteModifier {
+private extension SwipeActionModifier {
     func setStage(translation: CGSize) {
         let totalTranslation = (offset + translation.width) * -1 // flipping for ease of use
         var newStage: SwipeStage = .initial
-        
+
         switch totalTranslation {
-        case ..<limit:
+        case ..<limit.width:
             newStage = .initial
-        case limit..<actionLimit:
+        case limit.width...:
             newStage = .locked
-        case actionLimit...:
-            newStage = .commit
         default:
             break;
         }
-        
+
         self.stage = newStage
     }
-    
+
     // Called from @State stage changes through .onChange()
     private func stageChanged(old: SwipeStage, new: SwipeStage) {
         // fire feedback generator
@@ -157,10 +160,6 @@ private extension SwipeToDeleteModifier {
         switch (old, new) {
         case (.initial, .locked):
             fireFeedback(intensity: 0.8)
-        case (.locked, .commit):
-            fireFeedback(intensity: 1)
-        case (.commit, .locked):
-            fireFeedback(intensity: 0.5)
         case (.locked, .initial):
             fireFeedback(intensity: 0.5)
         default:
@@ -171,7 +170,7 @@ private extension SwipeToDeleteModifier {
 
 
 // MARK:- Feedback generator managing
-private extension SwipeToDeleteModifier {
+private extension SwipeActionModifier {
     // called on @GestureState isActive change through .onChange()
     func manageGenerator(isActive old: Bool, new: Bool) {
         switch (old, new) {
@@ -184,7 +183,7 @@ private extension SwipeToDeleteModifier {
             break
         }
     }
-    
+
     func fireFeedback(intensity: CGFloat) {
         feedback?.impactOccurred(intensity: intensity)
         feedback?.prepare()
@@ -193,25 +192,57 @@ private extension SwipeToDeleteModifier {
 
 // MARK:- Previews
 public struct SwipeToDeleteModifier_Previews: PreviewProvider {
+
+    private enum TestAction: String, Hashable {
+        case delete
+        case edit
+        case pin
+
+        var color: Color {
+            switch self {
+            case .delete:
+                    .red
+            case .edit:
+                    .blue
+            case .pin:
+                    .green
+            }
+        }
+        var title: String {
+            switch self {
+            case .delete:
+                "Delete"
+            case .edit:
+                "Edit"
+            case .pin:
+                "Pin"
+            }
+        }
+    }
+
+    static private func item(for action: TestAction, config: SwipeActionConfig, actionCount: Int = 3) -> some View {
+        Text(action.title)
+            .padding()
+            .frame(width: 90)
+            .frame(maxHeight: .infinity)
+            .background(action.color)
+            .foregroundColor(Color.white)
+            .offset(x: CGFloat(actionCount - config.itemIndex) * config.actionContainerWidth/CGFloat(actionCount) * (1 - config.progress))
+            .opacity(config.progress > 0 ? 1 : 0)
+    }
+
     public static var previews: some View {
-        Color.blue
+        Color.purple
             .frame(
                 width: 300,
                 height: 60
             )
             .modifier(
-                SwipeToDeleteModifier(
-                    onDelete: {},
-                    deleteView: { config in
-                        Text("Delete")
-                            .padding()
-                            .aspectRatio(1, contentMode: .fill)
-                            .background(Color.red)
-                            .foregroundColor(Color.white)
-                            .cornerRadius(8)
-                            .scaleEffect(config.progress)
-                    }
-                )
+                SwipeActionModifier(swipeActions: [TestAction.pin, TestAction.edit, TestAction.delete]) { action, config in
+                    item(for: action, config: config, actionCount: 3)
+                }
             )
+            .border(color: .red, corderRadius: 0, relationToContent: .over, lineWidth: 1)
     }
 }
+
